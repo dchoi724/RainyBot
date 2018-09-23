dfb3567a2ed177caf76f255042478a108c4b9458
Daniel Choi <danielchoi42@gmail.com>
2018-09-23 12:50:46 -0400

    add subscribers and unsubscribe option

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # raintel
+
+You must supply your own botconfig.json file containing the token and prefix
+{
+	"token": "[must get from your own Discord bot]",
+	"prefix": "rain>"
+}
+
+Or simply add my bot to your server: https://discordapp.com/oauth2/authorize?client_id=493450487941693455&permissions=134208&scope=bot
--- raintel.js
+++ raintel.js
@@ -6,18 +6,29 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+// TODO: replace with persistent data
 let subscribers = new Array(); // global list of subscribers
 
 // message lambda
 client.on('message', msg => {
-  if (msg.content === /^weather subscribe$/) {
-    msg.reply(msg.author.username + " subscribed to PM rain alerts.");
-    subscribers.push(msg.author);
-  }
-  if (msg.content === /^weather unsubscribe$/) {
-  	msg.reply(msg.author.username + " unsubscribed from PM rain alerts.");
-    subscribers = subscribers.filter((value, index, arr) => value.id === msg.author.id);
-  }
+	if(msg.author.bot) return;
+	if(msg.channel.type === "dm") return;
+
+	let prefix = botconfig.prefix;
+	let cmd = msg.content;
+
+	if (cmd === `${prefix} ping`) {
+		msg.channel.send("pong");
+	}
+	if (cmd === `${prefix} subscribe`) {
+		msg.channel.send(msg.author.username + " subscribed to PM rain alerts.");
+		subscribers.push(msg.author);
+	}
+	if (cmd === `${prefix} unsubscribe`) {
+		msg.channel.send(msg.author.username + " unsubscribed from PM rain alerts.");
+		subscribers = subscribers.filter((value, index, arr) => value.id !== msg.author.id);
+	}
 });
 
 // array methods
@@ -27,4 +38,4 @@ client.on('message', msg => {
 // actively PM users in subscribers list about weather
 
 
-client.login(config.token);
+client.login(botconfig.token);
